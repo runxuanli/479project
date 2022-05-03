9dee252de95237a283e63432e5041f3cff6470d4
Stephen <jling9@wisc.edu>
2022-05-03 13:29:12 -0500

    Update

--- initialize.sh
+++ initialize.sh
@@ -7,8 +7,7 @@ mkdir -p dag
 
 seq 1 5 > lists.txt
 
-echo "kNN" > files.txt
 echo "logistic" >> files.txt
 echo "naiveBayes" >> files.txt
-echo "svm" >> files.txt
 echo "randomForest" >> files.txt
+
