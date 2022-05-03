f0246a36241b4a86e2d26559e57eb5ecf7c7dd3f
Stephen <jling9@wisc.edu>
2022-05-03 14:46:46 -0500

    Update

--- var2.R
+++ var2.R
@@ -11,11 +11,19 @@
 # preparation
 rm(list=ls())
 library(mlr3verse)
+library(imbalance)
 
 # take cmd arg
 algorithm <- readLines("alg.txt")
-data.temp <- read.csv("data.csv")
+data.temp <- read.csv("data2.csv")
 data.temp$is_attributed <- as.factor(data.temp$is_attributed)
+
+data.temp <- oversample(
+  data.temp,
+  ratio = 0.8,
+  method = "SMOTE",
+  filtering = F,
+  classAttr = "is_attributed")
 
 var.comb <- function(alg, t) {
   # prepare data
